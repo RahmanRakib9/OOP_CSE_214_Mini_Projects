9d2d2f3a5bd92d6cce6b243e354d4280d4f696f1
RahmanRakib9 <rahmanrakibur6589@gmail.com>
2023-12-10 03:33:50 +0600

    import book,user and borrow book related classes into main class

--- Library_Management/src/MainClass.java
+++ Library_Management/src/MainClass.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainClass {
@@ -9,6 +10,9 @@ public class MainClass {
     public static void main(String[] args) {
         display("Welcome to Library Management Application");
 
+        ArrayList<Book> books = new ArrayList<>();
+        ArrayList<User> users = new ArrayList<>();
+
         while (true) {
             display("Enter 1 for registering a book");
             display("Enter 2 for show all books");
@@ -17,11 +21,34 @@ public class MainClass {
             display("Enter 5 for borrow a book");
             display("Enter 6 for return the book");
 
-            Scanner scanner=new Scanner(System.in);
-            int choice=scanner.nextInt();
+            Scanner scanner = new Scanner(System.in);
+            int choice = scanner.nextInt();
 
-            
+            // import classes where all the core functionalities are defined
+            BookRelatedOperation bookRelatedOperation = new BookRelatedOperation();
+            UserRelatedOperation userRelatedOperation = new UserRelatedOperation();
+            BorrowBook borrowBook = new BorrowBook();
 
+            switch (choice) {
+                case 1:
+                    books.add(bookRelatedOperation.register());
+                    break;
+                case 2:
+                    bookRelatedOperation.show(books);
+                    break;
+                case 3:
+                    users.add(userRelatedOperation.register());
+                    break;
+                case 4:
+                    userRelatedOperation.show(users);
+                    break;
+                case 5:
+                    borrowBook.borrow(users, books);
+                    break;
+                case 6:
+                    borrowBook.returnBook(users);
+                default:
+            }
 
         }
 
