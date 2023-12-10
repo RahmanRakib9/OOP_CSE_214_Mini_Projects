962e812ab7e383b01915b8f285bbb792961d4b1a
RahmanRakib9 <rahmanrakibur6589@gmail.com>
2023-12-10 13:42:49 +0600

    create a voter class that create a voter

--- Voting_System/src/MainClass.java
+++ Voting_System/src/MainClass.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
 
 /**
  * MainClass
  */
 public class MainClass {
+    public static void main(String[] args) {
+        // Display welcome message
+        System.out.println("===============");
+        System.out.println("Welcome to EVM");
+        System.out.println("===============");
 
-    
+        while (true) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Press 1 for Registration");
+            System.out.println("Press 2 for vote");
+            System.out.println("Press 0 for exit");
+            int choice = scan.nextInt();
+
+            
+
+        }
+
+    }
+
 }
---
+++ Voting_System/src/Voter.java
@@ -0,0 +1,57 @@
+/**
+ * This Voter class is responsible for create a new voter by voter name,NID and
+ * address
+ */
+public class Voter {
+    private String name;
+    private String NID;
+    private String address;
+
+    public Voter(String name, String NID, String address) {
+        this.name = name;
+        this.NID = NID;
+        this.address = address;
+    }
+
+    // name getter method
+    public String getName() {
+        return name;
+    }
+
+    // name setter method
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // NID getter method
+    public String getNID() {
+        return NID;
+    }
+
+    // NID setter method
+    public void setNID() {
+        this.NID = NID;
+    }
+
+    // address getter method
+    public String getAddress() {
+        return address;
+    }
+
+    // address setter method
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /*
+     * The purpose of this toString method is to create a string that represents the
+     * state of the User object. It concatenates the values of the name, NID, and
+     * address instance variables into a single string.
+     * 
+     */
+    @Override
+    public String toString() {
+        return "User [name=" + name + ", NID=" + NID + ", address=" + address + "]";
+    }
+
+}
